f862599375fa8d0b89f586abdc51090add66d71e
Yunus Mustapha <34034160+codeoracle@users.noreply.github.com>
2023-09-25 09:22:17 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,20 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_.API_KEY,
-  // authDomain: import.meta.env.VITE_.AUTH_DOMAIN,
-  // projectId: import.meta.env.VITE_.PROJECT_ID,
-  // storageBucket: import.meta.env.VITE_.STORAGE_BUCKET,
-  // messagingSenderId: import.meta.env.VITE_.MESSAGING_SENDER_ID,
-  // appId: import.meta.env.VITE_.APP_ID
 
-  apiKey: "AIzaSyA32uMAPKb1-kuvOhqfOnDQifsFu-Ev0uU",
-  authDomain: "image-gallery-7ea9a.firebaseapp.com",
-  projectId: "image-gallery-7ea9a",
-  storageBucket: "image-gallery-7ea9a.appspot.com",
-  messagingSenderId: "938682610183",
-  appId: "1:938682610183:web:b3a4fa06e2974e435070a2"
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_.API_KEY,
+  authDomain: import.meta.env.VITE_.AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_.PROJECT_ID,
+  storageBucket: import.meta.env.VITE_.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_.MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_.APP_ID
+
 };
 
 
